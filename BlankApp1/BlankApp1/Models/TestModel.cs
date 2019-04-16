34ada1d6c0e46155281fb14568f222e1242f991f
Bruno Henrique <bruno.henriquy@gmail.com>
2019-04-15 21:39:42 -0300

    Code

---
+++ BlankApp1/BlankApp1/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlankApp1.Models
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ BlankApp1/BlankApp1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,98 @@
+﻿using BlankApp1.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace BlankApp1.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<TestModel> TestList { get; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+            TestList = new ObservableCollection<TestModel>()
+            {
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"},
+                new TestModel(){Name = "Try"}
+            };
+
+        }
+    }
+}
